52f850bc71b3bc5e89600446512e2f3f2fb632de
Timur <tima.temirov.2005@mail.ru>
2024-05-19 10:53:44 -0400

    3

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -22,11 +22,11 @@ TEST(tests, test2) {
     std::streambuf* cout_streambuf = std::cout.rdbuf();
     std::cout.rdbuf(oss.rdbuf());
     SchoolGirl* girl = new SchoolGirl();
-    girl->setName("jiaojiao");
+    girl->setName("A");
     Proxy* proxy = new Proxy(girl);
     proxy->GiveFlowers();
     std::cout.rdbuf(cout_streambuf);
-    EXPECT_EQ(oss.str(), "A: give you you flowers\n");
+    EXPECT_EQ(oss.str(), "A: give you flowers\n");
 }
 
 TEST(tests, test3) {
@@ -34,10 +34,10 @@ TEST(tests, test3) {
     std::streambuf* cout_streambuf = std::cout.rdbuf();
     std::cout.rdbuf(oss.rdbuf());
     SchoolGirl* girl = new SchoolGirl();
-    girl->setName("jiaojiao");
+    girl->setName("B");
     Proxy* proxy = new Proxy(girl);
     proxy->GiveDolls();
     proxy->GiveFlowers();
     std::cout.rdbuf(cout_streambuf);
-    EXPECT_EQ(oss.str(), "A: give you a doll\nA: give you you flowers\n");
+    EXPECT_EQ(oss.str(), "B: give you a doll\nB: give you flowers\n");
 }
